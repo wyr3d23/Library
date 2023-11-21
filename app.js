1f93e10da04a2bf034b31889d76c4032c7708171
wyr3d23 <a.wells22@proton.me>
2023-11-20 20:04:34 -0500

    Added delete button, need to fix issues with content coming back, updated html page with additional content

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const addBook = document.querySelector('#add-book');
 
-function Book(title, author, pages) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
 }
 
-function addBookToLibrary(title, author, pages) {
-    const book = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, isRead) {
+    const book = new Book(title, author, pages, isRead);
 
     myLibrary.push(book);
     showLibrary();
@@ -38,7 +39,44 @@ function showLibrary() {
         const bookPages = document.createElement('td');
         bookPages.textContent = myLibrary[i].pages;
         bookRow.appendChild(bookPages);
+
+        const isRead = document.createElement('td');
+        isRead.textContent = myLibrary[i].isRead;
+        bookRow.appendChild(isRead);
+
+        const deleteRow = document.createElement('td');
+        deleteRow.classList.add('delete-row');
+        bookRow.appendChild(deleteRow);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.classList.add('delete-btn');
+        deleteRow.appendChild(deleteBtn);
     }
+}
+function clearEntry() {
+    const bookList = document.querySelector('.tableBody');
+    const bookRow = document.querySelector('tr');
+    bookList.removeChild(bookRow);
+
+    const bookTitle = document.querySelector('td');
+    bookRow.removeChild(bookTitle);
+
+    const bookAuthor = document.querySelector('td');
+    bookRow.removeChild(bookAuthor);
+
+    const bookPages = document.querySelector('td');
+    bookRow.removeChild(bookPages);
+
+    const isRead = document.querySelector('td');
+    bookRow.removeChild(isRead);
+
+    const deleteRow = document.querySelector('td');
+    bookRow.removeChild(deleteRow);
+
+    const deleteBtn = document.querySelector('button');
+    deleteRow.removeChild(deleteBtn);
+
 }
 
 
@@ -61,6 +99,8 @@ function bookListener() {
         const { target } = e;
         if (target.id === 'add-book') {
             validateForm(e);
+        } else if (target.className === 'delete-btn') {
+            clearEntry();
         }
         showLibrary();
     });
